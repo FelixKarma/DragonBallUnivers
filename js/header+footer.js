112bd83a52963c5f2278355be620437abddc2657
LaGuize <guilhem.dolet@gmail.com>
2023-12-06 12:28:06 +0100

    recup

--- js/header+footer.js
+++ js/header+footer.js
@@ -40,11 +40,11 @@ header[0].innerHTML = `
 
 let nav = document.createElement("nav");
 nav.innerHTML = `
-        <h2><a href="">Séries</a></h2>
-        <h2><a href="">Films</a></h2>
-        <h2><a href="">Fan Art</a></h2>
-        <h2><a href="">Jeux Vidéos</a></h2>
-        <h2><a href="Boutique.html">Boutique</a></h2>
+        <h2><a class="nav__link" href="séries.html">Séries</a></h2>
+        <h2><a class="nav__link" href="films.html">Films</a></h2>
+        <h2><a class="nav__link" href="fanArt.html">Fan Art</a></h2>
+        <h2><a class="nav__link" href="jeux.html">Jeux Vidéos</a></h2>
+        <h2><a class="nav__link" href="Boutique.html">Boutique</a></h2>
 `
 header[0].after(nav)
 
