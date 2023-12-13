da259970d1c52571c2aec07464df2342d3e20cc4
tractoupelle <caliezgatien@gmail.com>
2023-12-13 13:54:30 +0100

    inscription

--- js/Inscription-Login.js
+++ js/Inscription-Login.js
@@ -1,15 +1,30 @@
 // selectall type sub
 
-function pshhhhhhhhhwshwsh() {
-    let pseudoCo = document.getElementById("pseudoCo").value
-    let pswCo = document.getElementById("pswCo").value
+function pshhhhhhhhhwshwsh0() {
     
     let pseudoInsc = document.getElementById("pseudoInsc").value
     let mailInsc = document.getElementById("mailInsc").value
     let pswInsc = document.getElementById("pswInsc").value
     let conPswInsc = document.getElementById("conPswInsc").value
     console.log(pseudoCo + " " + pswCo);
-    if((pseudoCo && pswCo) || (pseudoInsc && mailInsc && pswInsc && conPswInsc)){
+    if(pseudoInsc && mailInsc && pswInsc && conPswInsc && pswInsc == conPswInsc){
+        document.getElementById("test").type= "button"
+    
+        setTimeout(function() {
+            document.location.href = "Inscription-Login.html";
+        },2400)
+    }
+    else if(pswInsc != conPswInsc){
+        
+    }
+}
+    
+function pshhhhhhhhhwshwsh() {
+    let pseudoCo = document.getElementById("pseudoCo").value
+    let pswCo = document.getElementById("pswCo").value
+    
+    console.log(pseudoCo + " " + pswCo);
+    if((pseudoCo && pswCo) ){
     let track = new Audio("../media/dbz_transfo_sound.mp3")
     track.play()
     
@@ -18,4 +33,3 @@ function pshhhhhhhhhwshwsh() {
         },2400)
     }
 }
-    
